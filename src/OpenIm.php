5307921cd7d2cc9a7bc04d3c5d9d0872ca93d505
suliang20 <suliang@163.com>
2018-02-24 01:03:33 +0800

    composer init

---
+++ src/OpenIm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: su
+ * Date: 2018/2/24
+ * Time: 0:51
+ */
+
+namespace baichuan;
+
+class OpenIm
+{
+    public function test()
+    {
+        echo 'test', PHP_EOL;
+    }
+}
