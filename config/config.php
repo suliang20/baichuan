1a1ceada8027b3aab89f41bf19cadd6daecca4bf
suliang20 <suliang@163.com>
2018-02-26 21:40:09 +0800

    添加用户

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: su
+ * Date: 2018/2/26
+ * Time: 14:40
+ */
+//  项目根目录
+defined('ROOT') or define('ROOT', realpath(dirname(dirname(__FILE__))) . '/');
+define('APPKEY', '');
+define('SECRET', '');
+define('OPEN_IM_USER_PREFIX', 'test_');
